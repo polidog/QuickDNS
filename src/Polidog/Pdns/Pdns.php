34dacff55f1951634b096f465998c521ab3b71ff
polidog <polidogs@gmail.com>
2013-04-06 04:46:40 +0900

    印伝と問題

--- src/Polidog/Pdns/Pdns.php
+++ src/Polidog/Pdns/Pdns.php
@@ -177,11 +177,20 @@ class Pdns {
 			$this->output("call lookupExternal","debug");
 			return $this->lookupExternal($buffer, $client_ip, $client_port);
 		}
+
 		
-        $answer = $buffer[0].$buffer[1].chr(129).chr(128).$buffer[4].$buffer[5].$buffer[4].$buffer[5].chr(0).chr(0).chr(0).chr(0);
-        $answer .= substr($buffer, 12);
-        $answer .= chr(192).chr(12);
-        $answer .= chr(0).chr(1).chr(0).chr(1).chr(0).chr(0).chr(0).chr(60).chr(0).chr(4);
+		
+		
+		
+		
+		
+		
+		
+$answer = $buffer[0].$buffer[1].chr(129).chr(128).$buffer[4].$buffer[5].$buffer[4].$buffer[5].chr(0).chr(0).chr(0).chr(0);
+$answer .= substr($buffer, 12);
+$answer .= chr(192).chr(12);
+$answer .= chr(0).chr(1).chr(0).chr(1).chr(0).chr(0).chr(0).chr(60).chr(0).chr(4);
+		
 		
 		$_ip ="";
 		foreach(explode(".",$ip) as $v)	$_ip.=chr($v);
