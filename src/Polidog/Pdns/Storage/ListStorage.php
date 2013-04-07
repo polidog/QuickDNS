46949c88d0b3b1a266fd11009b6a0f76d7b161d9
polidog <polidogs@gmail.com>
2013-04-07 11:16:48 +0900

    domainList系のオブジェクトの影響範囲を狭くする

--- src/Polidog/Pdns/Storage/ListStorage.php
+++ src/Polidog/Pdns/Storage/ListStorage.php
@@ -1,14 +1,32 @@
 <?php
 namespace Polidog\Pdns\Storage;
+use Polidog\Pdns\Domain\DomainList;
+use \Polidog\Pdns\Domain\Domain;
+
 class ListStorage extends StorageAbstract {
+	
+	protected $DomainList;
+	
 	
 	public function __construct($config) {
 		parent::__construct($config);
+		$this->DomainList = new DomainList();
+		
 		if (isset($config['data']) && is_array($config['data'])) {
 			foreach ($config['data'] as $domain => $ip) {
 				$this->set($domain, $ip);
 			}
 		}
+		
+	}
+	
+	public function get($domain) {
+		return $this->DomainList->searchDomain($domain);
+	}
+	
+	public function set($domain, $ip, $expir = -1) {
+		$this->DomainList->set(new Domain($domain,$ip,$expir));
+		return $this;
 	}
 	
 	public function cache($domain, $ip, $expir = 3600) {
--- src/Polidog/Pdns/Storage/StorageAbstract.php
+++ src/Polidog/Pdns/Storage/StorageAbstract.php
@@ -1,7 +1,6 @@
 <?php
 namespace Polidog\Pdns\Storage;
-use Polidog\Pdns\Domain\DomainList;
-use \Polidog\Pdns\Domain\Domain;
+
 /**
  * ストレージ抽象化クラス
  * @property array $config 設定情報
@@ -10,11 +9,9 @@ use \Polidog\Pdns\Domain\Domain;
 abstract class StorageAbstract {
 	
 	protected $config = array();
-	protected $DomainList;
 	
 	public function __construct($config) {
 		$this->config = $config;
-		$this->DomainList = new DomainList();
 	}
 	
 	/**
@@ -22,19 +19,18 @@ abstract class StorageAbstract {
 	 * @param string $domain
 	 * @param string $ip
 	 */
-	public function set($domain,$ip, $expir = -1) {
-		$this->DomainList->set(new Domain($domain,$ip,$expir));
-		return $this;
-	}
+	abstract public function set($domain,$ip, $expir = -1);
 		
 	/**
 	 * ドメイン情報を取得する
 	 * @param string $domain
 	 * @return Domain;
 	 */
-	public function get($domain) {
-		return $this->DomainList->searchDomain($domain);
-	}
+	abstract public function get($domain);
 	
+	
+	/**
+	 * キャッシュを生成する
+	 */
 	abstract function cache($domain,$ip,$expir);
 }
