fabc65b8e23c3ac22923565c6bffcc12cd978d2c
polidog <polidogs@gmail.com>
2013-04-12 00:13:03 +0900

    サンプルの修正

--- examples/dns_server.php
+++ examples/dns_server.php
@@ -1,17 +1,14 @@
 <?php
+// listタイプのストレージを使ったDNSサーバの起動
 require '../vendor/autoload.php';
 use Polidog\Pdns\Pdns;
 use Polidog\Pdns\Exception\PdnsDomainException;
 
 $server = new Pdns();
 $server->init(function() use ($server) {
-	$server->setStorageConfig(array(
-		'type' => 'List',
-		'data' => array(
-			'www.polidog.jp' => '133.242.145.155'
-		),
-	));
-	
+	$server->setStorage(new \Polidog\Pdns\Storage\ListStorage(array(
+		'www.polidog.jp' => '133.242.145.155'
+	)));
 	$server->set('stdout', true);
 });
 
